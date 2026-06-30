99b0d40f93b2f9542ff42bd433b85886d65ae154
Dmitriy Lazarev <w@kich.dev>
2026-06-30 14:22:00 +0500

    feat(storybook): add AdminAnnounceSection stories

---
+++ client/settings/sections/admin/AdminAnnounceSection.stories.svelte
@@ -0,0 +1,17 @@
+<!-- SPDX-License-Identifier: BUSL-1.1 -->
+<!-- Copyright (c) 2026 Dmitriy Lazarev -->
+<!-- Use of this software is governed by the Business Source License 1.1. -->
+<!-- See LICENSE in the project root for details. -->
+
+<script module lang="ts">
+  import { defineMeta } from '@storybook/addon-svelte-csf'
+
+  import AdminAnnounceSection from './AdminAnnounceSection.svelte'
+
+  const { Story } = defineMeta({
+    title: 'settings/sections/admin/AdminAnnounceSection',
+    component: AdminAnnounceSection,
+  })
+</script>
+
+<Story name="Default" />
